2260f3619540d0392625ac4d106b262cc16596f8
820514 <820514@nd>
2017-04-07 14:10:19 +0800

    update

---
+++ 2017-04-07/currying/1/script.js
@@ -0,0 +1,34 @@
+/**
+ * 柯里化
+ * @param {Function} fn 函数
+ * @return {Function}
+ */
+const currying = function (fn) {
+  let args = []
+
+  return function () {
+    if (!!arguments.length) {
+      [].push.apply(args, arguments)
+      return arguments.callee
+    } else {
+      return fn.apply(null, args)
+    }
+  }
+}
+
+// 求和
+const sum = (function (num) {
+  let total = 0
+
+  return function () {
+    for (let i = 0; i < arguments.length; i++) {
+      total += arguments[i]
+    }
+
+    return total
+  }
+})()
+
+const total = currying(sum)(1, 2)(3)(4)()
+
+console.log(total)
